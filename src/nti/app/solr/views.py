709652391bfedd2791a8952778b90a82a65a347f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-21 16:47:56 +0000

    helper view to index a content unit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@99175 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/solr/views.py
+++ src/nti/app/solr/views.py
@@ -11,9 +11,24 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
+from zope.event import notify
+
 from zope.container.contained import Contained
 
 from zope.traversing.interfaces import IPathAdapter
+
+from nti.contentlibrary.interfaces import IContentUnit
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.dataserver import authorization as nauth
+
+from nti.solr.interfaces import IndexObjectEvent
 
 @interface.implementer(IPathAdapter)
 class SOLRPathAdapter(Contained):
@@ -24,3 +39,15 @@ class SOLRPathAdapter(Contained):
 		self.context = context
 		self.request = request
 		self.__parent__ = context
+
+@view_config(name='solr_index')
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   request_method='POST',
+			   context=IContentUnit,
+			   permission=nauth.ACT_NTI_ADMIN)
+class IndexContentUnitView(AbstractAuthenticatedView):
+
+	def __call__(self):
+		notify(IndexObjectEvent(self.context))
+		return hexc.HTTPNoContent()
