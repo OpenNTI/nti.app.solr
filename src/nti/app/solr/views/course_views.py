e3df44b89187b2ee10c9cf901847360a4ae44a10
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-07 23:47:13 +0000

    index all courses
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@100262 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/solr/views/course_views.py
+++ src/nti/app/solr/views/course_views.py
@@ -9,15 +9,21 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import component
+
 from pyramid import httpexceptions as hexc
 
 from pyramid.view import view_config
 from pyramid.view import view_defaults
 
+from nti.app.solr.views import SOLRPathAdapter
+
 from nti.app.solr.views.general_views import SOLRIndexObjectView
 
+from nti.contenttypes.courses.interfaces import ICourseCatalog
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+from nti.contenttypes.courses.legacy_catalog import ILegacyCourseCatalogEntry
 
 from nti.contenttypes.courses.utils import get_course_subinstances
 
@@ -36,4 +42,21 @@ class IndexCourseView(SOLRIndexObjectView):
 		self._notify(ICourseInstance(self.context))
 		for course in get_course_subinstances(self.context):
 			self._notify(course)
+		return hexc.HTTPNoContent()
+
+@view_config(name='IndexAllCourses')
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   request_method='POST',
+			   context=SOLRPathAdapter,
+			   permission=nauth.ACT_NTI_ADMIN)
+class IndexAllCoursesView(SOLRIndexObjectView):
+
+	def __call__(self):
+		catalog = component.queryUtility(ICourseCatalog)
+		if catalog is not None:
+			for entry in catalog.iterCatalogEntries():
+				if not ILegacyCourseCatalogEntry.providedBy(entry):
+					course = ICourseInstance(entry)
+					self._notify(course)
 		return hexc.HTTPNoContent()
