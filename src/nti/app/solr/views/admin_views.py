971255ae3d99dae67fe839ee87d936bb3693c267
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-06 22:35:38 +0000

    add view to get jobs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@102102 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/solr/views/admin_views.py
+++ src/nti/app/solr/views/admin_views.py
@@ -20,9 +20,36 @@ from nti.app.solr.views import SOLRPathAdapter
 
 from nti.dataserver import authorization as nauth
 
+from nti.externalization.interfaces import LocatedExternalDict
+from nti.externalization.interfaces import StandardExternalFields
+
 from nti.solr import QUEUE_NAMES
 
 from nti.solr.common import get_job_queue
+
+ITEMS = StandardExternalFields.ITEMS
+TOTAL = StandardExternalFields.TOTAL
+ITEM_COUNT = StandardExternalFields.ITEM_COUNT
+
+@view_config(name='Jobs')
+@view_config(name='jobs')
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   request_method='GET',
+			   context=SOLRPathAdapter,
+			   permission=nauth.ACT_NTI_ADMIN)
+class QueueJobsView(AbstractAuthenticatedView):
+
+	def __call__(self):
+		total = 0
+		result = LocatedExternalDict()
+		items = result[ITEMS] = {}
+		for name in QUEUE_NAMES:
+			queue = get_job_queue(name)
+			items[name] = list(queue.keys())  # snapshopt
+			total += len(items[name])
+		result[TOTAL] = result[ITEM_COUNT] = total
+		return result
 
 @view_config(name='EmptyQueues')
 @view_config(name='empty_queues')
