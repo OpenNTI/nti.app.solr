166bc15c988817bf80278a76c75f288ad22d3879
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-13 20:52:34 +0000

    sync packages
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@102507 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/solr/scripts/initializer.py
+++ src/nti/app/solr/scripts/initializer.py
@@ -104,6 +104,10 @@ class _SolrInitializer(object):
 			from nti.contentlibrary.interfaces import IContentPackageLibrary
 			catalog = component.queryUtility(IContentPackageLibrary)
 			if catalog is not None:
+				try:
+					catalog.syncContentPackages()
+				except AttributeError:
+					pass
 				for package in catalog.contentPackages or ():
 					if not IGlobalContentPackage.providedBy(package):
 						yield package
