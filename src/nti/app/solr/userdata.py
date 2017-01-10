26d460677b077712b7d8629432575796888948f6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-10 21:27:36 +0000

    use data is not site based
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@103983 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/solr/userdata.py
+++ src/nti/app/solr/userdata.py
@@ -11,8 +11,6 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 
-from zope.component.hooks import site as current_site
-
 from zope.intid.interfaces import IIntIds
 
 from nti.dataserver.interfaces import IUser
@@ -27,55 +25,56 @@ from nti.solr import USERDATA_QUEUE
 
 from nti.solr.common import finder
 from nti.solr.common import add_to_queue
-from nti.solr.common import get_job_site
 
 from nti.solr.interfaces import ICoreCatalog
 from nti.solr.interfaces import IIndexObjectEvent
 
+
 def all_user_generated_data(users=(), sharedWith=False):
-	intids = component.getUtility(IIntIds)
-	catalog = dataserver_metadata_catalog()
-	usernames = {getattr(user, 'username', user).lower() for user in users or ()}
-	if usernames:
-		uids = catalog[IX_CREATOR].apply({'any_of': usernames})
-	else:
-		uids = catalog[IX_CREATOR].ids()
+    intids = component.getUtility(IIntIds)
+    catalog = dataserver_metadata_catalog()
+    usernames = {getattr(user, 'username', user).lower()
+                 for user in users or ()}
+    if usernames:
+        uids = catalog[IX_CREATOR].apply({'any_of': usernames})
+    else:
+        uids = catalog[IX_CREATOR].ids()
 
-	for uid in uids or ():
-		obj = intids.queryObject(uid)
-		if IUserGeneratedData.providedBy(obj):
-			yield uid, obj
+    for uid in uids or ():
+        obj = intids.queryObject(uid)
+        if IUserGeneratedData.providedBy(obj):
+            yield uid, obj
 
-	if usernames and sharedWith:
-		intids_sharedWith = catalog[IX_SHAREDWITH].apply({'any_of': usernames})
-		for uid in intids_sharedWith or ():
-			obj = intids.queryObject(uid)
-			if IUserGeneratedData.providedBy(obj):
-				yield uid, obj
-			
+    if usernames and sharedWith:
+        intids_sharedWith = catalog[IX_SHAREDWITH].apply({'any_of': usernames})
+        for uid in intids_sharedWith or ():
+            obj = intids.queryObject(uid)
+            if IUserGeneratedData.providedBy(obj):
+                yield uid, obj
+
+
 def process_userdata(user, index=True):
-	for _, obj in all_user_generated_data(users=(user,)):
-		catalog = ICoreCatalog(obj)
-		operation = catalog.add if index else catalog.remove
-		operation(obj, commit=False)  # wait for server to commit
+    for _, obj in all_user_generated_data(users=(user,)):
+        catalog = ICoreCatalog(obj)
+        operation = catalog.add if index else catalog.remove
+        operation(obj, commit=False)  # wait for server to commit
+
 
 def index_userdata(source, site=None, *args, **kwargs):
-	job_site = get_job_site(site)
-	with current_site(job_site):
-		obj = IUser(finder(source), None)
-		if IUser.providedBy(obj):
-			logger.info("Indexing data for user %s started", obj)
-			process_userdata(obj, index=True)
-			logger.info("Indexing data for user %s completed", obj)
+    obj = IUser(finder(source), None)
+    if IUser.providedBy(obj):
+        logger.info("Indexing data for user %s started", obj)
+        process_userdata(obj, index=True)
+        logger.info("Indexing data for user %s completed", obj)
+
 
 def unindex_userdata(source, site=None, *args, **kwargs):
-	job_site = get_job_site(site)
-	with current_site(job_site):
-		obj = IUser(finder(source), None)
-		if IUser.providedBy(obj):
-			process_userdata(obj, index=False)
+    obj = IUser(finder(source), None)
+    if IUser.providedBy(obj):
+        process_userdata(obj, index=False)
+
 
 @component.adapter(IUser, IIndexObjectEvent)
 def _index_user(obj, event):
-	add_to_queue(USERDATA_QUEUE, index_userdata, obj=obj,
-				 jid='userdata_indexing')
+    add_to_queue(USERDATA_QUEUE, index_userdata, obj=obj,
+                 jid='userdata_indexing')
