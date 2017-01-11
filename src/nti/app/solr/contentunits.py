eeed1a324b2c8ffc3a023bdb250da6cbad87d3f6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-11 21:04:38 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@104104 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/solr/contentunits.py
+++ src/nti/app/solr/contentunits.py
@@ -27,42 +27,49 @@ from nti.solr.interfaces import ICoreCatalog
 from nti.solr.interfaces import IIndexObjectEvent
 from nti.solr.interfaces import IUnindexObjectEvent
 
+
 def process_content_package_evaluations(obj, index=True):
-	collector = set()
-	def recur(unit):
-		container = IQAssessmentItemContainer(unit, None)
-		if container:
-			collector.update(container.values())
-		for child in unit.children or ():
-			recur(child)
-	recur(obj)
-	for a in collector:
-		catalog = ICoreCatalog(a)
-		operation = catalog.add if index else catalog.remove
-		operation(a, commit=False)  # wait for server to commit
+    collector = set()
+
+    def recur(unit):
+        container = IQAssessmentItemContainer(unit, None)
+        if container:
+            collector.update(container.values())
+        for child in unit.children or ():
+            recur(child)
+    recur(obj)
+    for a in collector:
+        catalog = ICoreCatalog(a)
+        operation = catalog.add if index else catalog.remove
+        operation(a, commit=False)  # wait for server to commit
+
 
 def index_content_package_evaluations(source, site=None, *args, **kwargs):
-	job_site = get_job_site(site)
-	with current_site(job_site):
-		obj = finder(source)
-		if IContentPackage.providedBy(obj):
-			logger.info("Content package %s evaluations indexing started", obj.ntiid)
-			process_content_package_evaluations(obj, index=True)
-			logger.info("Content package %s evaluations indexing completed", obj.ntiid)
+    job_site = get_job_site(site)
+    with current_site(job_site):
+        obj = finder(source)
+        if IContentPackage.providedBy(obj):
+            ntiid = obj.ntiid
+            logger.info("Content package %s evaluations indexing started", ntiid)
+            process_content_package_evaluations(obj, index=True)
+            logger.info("Content package %s evaluations indexing completed", ntiid)
+
 
 def unindex_content_package_evaluations(source, site=None, *args, **kwargs):
-	job_site = get_job_site(site)
-	with current_site(job_site):
-		obj = finder(source)
-		if IContentPackage.providedBy(obj):
-			process_content_package_evaluations(obj, index=False)
+    job_site = get_job_site(site)
+    with current_site(job_site):
+        obj = finder(source)
+        if IContentPackage.providedBy(obj):
+            process_content_package_evaluations(obj, index=False)
+
 
 @component.adapter(IContentPackage, IIndexObjectEvent)
 def _index_contentpackage(obj, event):
-	add_to_queue(EVALUATIONS_QUEUE,
-				 index_content_package_evaluations, obj, jid='evaluations_added')
+    add_to_queue(EVALUATIONS_QUEUE,
+                 index_content_package_evaluations, obj, jid='evaluations_added')
+
 
 @component.adapter(IContentPackage, IUnindexObjectEvent)
 def _unindex_contentpackage(obj, event):
-	add_to_queue(EVALUATIONS_QUEUE, unindex_content_package_evaluations, obj,
-				 jid='evaluations_removed')
+    add_to_queue(EVALUATIONS_QUEUE, unindex_content_package_evaluations, obj,
+                 jid='evaluations_removed')
