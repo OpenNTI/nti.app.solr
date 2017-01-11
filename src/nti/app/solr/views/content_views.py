d290fc219b87aae5ce323bdf2eb9bfff7b815c19
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-11 17:43:02 +0000

    svn sucks
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@104084 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/solr/views/content_views.py
+++ src/nti/app/solr/views/content_views.py
@@ -26,57 +26,60 @@ from nti.contentlibrary.interfaces import IContentPackageLibrary
 
 from nti.dataserver import authorization as nauth
 
+
 @view_config(context=IContentUnit)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   name='solr_index',
-			   request_method='POST',
-			   permission=nauth.ACT_NTI_ADMIN)
+               renderer='rest',
+               name='solr_index',
+               request_method='POST',
+               permission=nauth.ACT_NTI_ADMIN)
 class IndexObjectView(SOLRIndexObjectView):
 
-	def __call__(self):
-		self._notify(self.context)
-		return hexc.HTTPNoContent()
+    def __call__(self):
+        self._notify(self.context)
+        return hexc.HTTPNoContent()
+
 
 @view_config(context=IContentUnit)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   name='solr_unindex',
-			   request_method='POST',
-			   permission=nauth.ACT_NTI_ADMIN)
+               renderer='rest',
+               name='solr_unindex',
+               request_method='POST',
+               permission=nauth.ACT_NTI_ADMIN)
 class UnindexObjectView(UnindexSOLRObjectView):
 
-	def __call__(self):
-		self._notify(self.context)
-		return hexc.HTTPNoContent()
+    def __call__(self):
+        self._notify(self.context)
+        return hexc.HTTPNoContent()
 
 
 @view_config(name='IndexAllContentPackages')
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   request_method='POST',
-			   context=SOLRPathAdapter,
-			   permission=nauth.ACT_NTI_ADMIN)
+               renderer='rest',
+               request_method='POST',
+               context=SOLRPathAdapter,
+               permission=nauth.ACT_NTI_ADMIN)
 class IndexAllContentPackagesView(SOLRIndexObjectView):
 
-	def predicate(self, context):
-		return not IGlobalContentPackage.providedBy(context)
+    def predicate(self, context):
+        return not IGlobalContentPackage.providedBy(context)
 
-	def __call__(self):
-		library = component.queryUtility(IContentPackageLibrary)
-		if library is not None:
-			for package in library.contentPackages or ():
-				if self.predicate(package):
-					self._notify(package)
-		return hexc.HTTPNoContent()
+    def __call__(self):
+        library = component.queryUtility(IContentPackageLibrary)
+        if library is not None:
+            for package in library.contentPackages or ():
+                if self.predicate(package):
+                    self._notify(package)
+        return hexc.HTTPNoContent()
+
 
 @view_config(name='IndexLegacyContentPackages')
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   request_method='POST',
-			   context=SOLRPathAdapter,
-			   permission=nauth.ACT_NTI_ADMIN)
+               renderer='rest',
+               request_method='POST',
+               context=SOLRPathAdapter,
+               permission=nauth.ACT_NTI_ADMIN)
 class IndexLegacyContentPackagesView(IndexAllContentPackagesView):
 
-	def predicate(self, context):
-		return IGlobalContentPackage.providedBy(context)
+    def predicate(self, context):
+        return IGlobalContentPackage.providedBy(context)
