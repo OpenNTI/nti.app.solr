7f51dca90ad3feb33e4719b62616c0cdac60a68f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-03 17:11:54 +0000

    whitespace
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@105775 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,38 +4,38 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	"z3c.autoinclude.plugin": [
-		'target = nti.app',
-	],
-	"console_scripts": [
-		"nti_solr_initializer = nti.app.solr.scripts.initializer:main",
-	],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
+    "console_scripts": [
+        "nti_solr_initializer = nti.app.solr.scripts.initializer:main",
+    ],
 }
 
 setup(
-	name='nti.app.solr',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI SOLR App",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='pyramid preference',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.app'],
-	install_requires=[
-		'setuptools',
-		'nti.solr'
-	],
-	entry_points=entry_points
+    name='nti.app.solr',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI SOLR App",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='pyramid preference',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti', 'nti.app'],
+    install_requires=[
+        'setuptools',
+        'nti.solr'
+    ],
+    entry_points=entry_points
 )
