d06261088a945798de23606b7fa996707282b443
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-09 00:25:03 +0000

    add gen install test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@106144 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/solr/tests/__init__.py
+++ src/nti/app/solr/tests/__init__.py
@@ -6,3 +6,38 @@ __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
+
+from nti.testing.layers import GCLayerMixin
+from nti.testing.layers import ZopeComponentLayer
+from nti.testing.layers import ConfiguringLayerMixin
+
+from nti.dataserver.tests.mock_dataserver import DSInjectorMixin
+
+import zope.testing.cleanup
+
+
+class SharedConfiguringTestLayer(ZopeComponentLayer,
+                                 GCLayerMixin,
+                                 ConfiguringLayerMixin,
+                                 DSInjectorMixin):
+
+    set_up_packages = ('nti.dataserver',
+                       'nti.solr',
+                       'nti.app.solr')
+
+    @classmethod
+    def setUp(cls):
+        cls.setUpPackages()
+
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        zope.testing.cleanup.cleanUp()
+
+    @classmethod
+    def testSetUp(cls, test=None):
+        cls.setUpTestDS(test)
+
+    @classmethod
+    def testTearDown(cls):
+        pass
