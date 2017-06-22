3709fb845131dfd850cf07fae9e5759ff798255c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-22 15:59:52 +0000

    add supporting files
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@115734 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ README.rst
@@ -0,0 +1,9 @@
+==============
+ nti.app.solr
+==============
+
+.. image:: https://travis-ci.org/NextThought/nti.app.solr.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.solr
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.solr/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.solr?branch=master
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import codecs
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
 
 entry_points = {
     "z3c.autoinclude.plugin": [
@@ -12,15 +11,21 @@ entry_points = {
     ],
 }
 
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
     name='nti.app.solr',
-    version=VERSION,
+    version=_read('version.txt').strip(),
     author='Jason Madden',
     author_email='jason@nextthought.com',
     description="NTI SOLR App",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='pyramid preference',
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='pyramid solr index',
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
@@ -29,12 +34,15 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
+    zip_safe=True,
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    include_package_data=True,
     namespace_packages=['nti', 'nti.app'],
+    tests_require=TESTS_REQUIRE,
     install_requires=[
         'setuptools',
-        'nti.solr'
+        'nti.solr',
     ],
-    entry_points=entry_points
+    entry_points=entry_points,
 )
