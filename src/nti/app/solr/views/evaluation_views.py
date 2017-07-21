43540e8741056fdc2eedf839134ae095ab855609
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-21 03:24:23 +0000

    add views to index/unindex evals
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@117912 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/solr/views/evaluation_views.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+
+from pyramid import httpexceptions as hexc
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.app.solr.views import SOLRPathAdapter
+
+from nti.app.solr.views.general_views import SOLRIndexObjectView
+from nti.app.solr.views.general_views import UnindexSOLRObjectView
+
+from nti.assessment.interfaces import IQEvaluation
+
+from nti.dataserver import authorization as nauth
+
+
+@view_config(context=IQEvaluation)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               name='solr_index',
+               request_method='POST',
+               permission=nauth.ACT_NTI_ADMIN)
+class IndexEvaluationView(SOLRIndexObjectView):
+
+    def __call__(self):
+        self._notify(self.context)
+        return hexc.HTTPNoContent()
+
+
+@view_config(context=IQEvaluation)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               name='solr_unindex',
+               request_method='POST',
+               permission=nauth.ACT_NTI_ADMIN)
+class UnindexEvaluationView(UnindexSOLRObjectView):
+
+    def __call__(self):
+        self._notify(self.context)
+        return hexc.HTTPNoContent()
+
+
+@view_config(name='IndexAllEvaluations')
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               request_method='POST',
+               context=SOLRPathAdapter,
+               permission=nauth.ACT_NTI_ADMIN)
+class IndexAllEvaluationsView(SOLRIndexObjectView):
+
+    def __call__(self):
+        for _, obj in component.getUtilitiesFor(IQEvaluation):
+            self._notify(obj)
+        return hexc.HTTPNoContent()
