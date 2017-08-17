cc44d7601f508719553a00d92bbea2d4beb4289a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-17 23:23:02 +0000

    add generation 5 to reindex the evaluation objects since their change
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.solr/trunk@119833 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/solr/generations/evolve5.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 5
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import setHooks
+from zope.component.hooks import site as current_site
+
+from zope.event import notify
+
+from zope.intid.interfaces import IIntIds
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.site.hostpolicy import get_all_host_sites
+
+from nti.solr.interfaces import ICoreCatalog
+from nti.solr.interfaces import IndexObjectEvent
+
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warn("Using dataserver without a proper ISiteManager.")
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
+
+def _sync_library():
+    try:
+        from nti.contentlibrary.interfaces import IContentPackageLibrary
+        library = component.queryUtility(IContentPackageLibrary)
+        if library is not None:
+            library.syncContentPackages()
+    except ImportError:
+        pass
+
+
+def _process_site(seen, intids):
+    try:
+        from nti.assessment.interfaces import IQEvaluation
+        for item in component.getUtilitiesFor(IQEvaluation):
+            doc_id = intids.queryId(item)
+            if doc_id is None or doc_id in seen:
+                continue
+            seen.add(doc_id)
+            notify(IndexObjectEvent(item))
+    except ImportError:
+        pass
+
+
+def do_evolve(context, generation=generation):
+    logger.info("SOLR evolution %s started", generation)
+
+    setHooks()
+    conn = context.connection
+    ds_folder = conn.root()['nti.dataserver']
+
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
+
+    with current_site(ds_folder):
+        assert component.getSiteManager() == ds_folder.getSiteManager(), \
+               "Hooks not installed?"
+
+        seen = set()
+        lsm = ds_folder.getSiteManager()
+        intids = lsm.getUtility(IIntIds)
+        # sync library
+        _sync_library()
+        # clear all evaluations
+        catalog = component.queryUtility(ICoreCatalog, name="evaluations")
+        if catalog is not None:
+            catalog.clear(commit=False)
+            for site in get_all_host_sites():
+                with current_site(site):
+                    _process_site(seen, intids)
+
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+    logger.info('SOLR Evolution %s done', generation)
+
+
+def evolve(context):
+    """
+    Evolve to generation 5 by reindexing the evaluation objects
+    """
+    do_evolve(context)
