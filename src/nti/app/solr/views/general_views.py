224f2aeab760e79085cbfad254f1292ab6ada0d8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-06 16:40:09 -0500

    clean up;

--- src/nti/app/solr/views/general_views.py
+++ src/nti/app/solr/views/general_views.py
@@ -8,16 +8,16 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import six
-
-from requests.structures import CaseInsensitiveDict
-
-from zope.event import notify
-
 from pyramid import httpexceptions as hexc
 
 from pyramid.view import view_config
 from pyramid.view import view_defaults
+
+from requests.structures import CaseInsensitiveDict
+
+import six
+
+from zope.event import notify
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
